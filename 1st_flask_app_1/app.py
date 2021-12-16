60deb8d5ff92b56becbc7fae34b9b19d8c88aa76
bupark82 <bupark82@gmail.com>
2021-12-16 21:08:33 +0900

    commit 1st flask app revision 2

--- 1st_flask_app_1/app.py
+++ 1st_flask_app_1/app.py
@@ -1,22 +1,33 @@
 from flask import Flask, render_template
-from cleanformer.datamodules import Kor2EngSmallDataModule
-from cleanformer.fetchers import fetch_config
+import argparse
+from cleanformer.builders import InferInputsBuilder
+from cleanformer.fetchers import fetch_config, fetch_tokenizer, fetch_transformer
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    ver = "overfit_small"
-    config = fetch_config()['train'][ver]
-    config.update({'num_workers': 2})
-    datamodule = Kor2EngSmallDataModule(config, None)  # noqa
-    datamodule.prepare_data()
-    # --- explore some data --- #
-    for pair in datamodule.kor2eng_train:
-        #print(pair)
-        pair
+    parser = argparse.ArgumentParser()
+    # you must provide this
+    parser.add_argument("entity", type=str, help="a wandb entity to download artifacts from")
+    parser.add_argument("--ver", type=str, default="overfit_small")
+    parser.add_argument("--kor", type=str, default="카페인은 원래 커피에 들어있는 물질이다.")
+    args = parser.parse_args()
+    config = fetch_config()['train'][args.ver]
+    config.update(vars(args))
+    # fetch a pre-trained transformer & and a pre-trained tokenizer
+    transformer = fetch_transformer(config['entity'], config['ver'])
+    tokenizer = fetch_tokenizer(config['entity'], config['tokenizer'])
+    transformer.eval()  # otherwise, the result will be different on every run
+    X = InferInputsBuilder(tokenizer, config['max_length'])(srcs=[config['kor']])
+    src_ids = X[0, 0, 0].tolist()  # (1, 2, 2, L) -> (L) -> list
+    pred_ids = transformer.predict(X).squeeze().tolist()  # (1, L) -> (L) -> list
+    pred_ids = pred_ids[: pred_ids.index(tokenizer.eos_token_id)]  # noqa
+    # print(tokenizer.decode(ids=src_ids), "->", tokenizer.decode(ids=pred_ids))
+    kr = tokenizer.decode(ids=src_ids)
+    us = tokenizer.decode(ids=pred_ids)
 
-    return render_template('first_app.html', pair=pair)
+    return render_template('first_app.html', kr=kr, us=us)
 
 if __name__ == '__main__':
     app.run(debug=True)
