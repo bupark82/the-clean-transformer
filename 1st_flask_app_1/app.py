c46cb1d7415b7e88cb661ea1e3dc5b29c48c6b11
bupark82 <bupark82@gmail.com>
2021-12-16 17:36:53 +0900

    commit revision 1

--- 1st_flask_app_1/app.py
+++ 1st_flask_app_1/app.py
@@ -1,10 +1,22 @@
 from flask import Flask, render_template
+from cleanformer.datamodules import Kor2EngSmallDataModule
+from cleanformer.fetchers import fetch_config
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('first_app.html')
+    ver = "overfit_small"
+    config = fetch_config()['train'][ver]
+    config.update({'num_workers': 2})
+    datamodule = Kor2EngSmallDataModule(config, None)  # noqa
+    datamodule.prepare_data()
+    # --- explore some data --- #
+    for pair in datamodule.kor2eng_train:
+        #print(pair)
+        pair
+
+    return render_template('first_app.html', pair=pair)
 
 if __name__ == '__main__':
     app.run(debug=True)
